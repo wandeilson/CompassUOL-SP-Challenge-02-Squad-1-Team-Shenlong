f23c182a5a60ce15550938f813e3b109747de772
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2023-06-05 19:34:46 -0300

    AdAdding necessary dependencies, configuring application.properties, creating controller e saving one product on database for testing.

--- ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/model/Product.java
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/model/Product.java
@@ -1,10 +1,63 @@
 package br.com.compassuol.sp.challenge.ecommerce.model;
-
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
+@Entity
 public class Product {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
+    @Size(min = 3)
+    @NotBlank(message = "Name is mandatory")
     private String name;
+    @NotNull(message = "Name is mandatory")
     private BigDecimal price;
+    @Size(min = 3)
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
