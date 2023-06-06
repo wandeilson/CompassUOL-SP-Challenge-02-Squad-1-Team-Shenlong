d52b636bfadcf1973d53485ae55b2bff8ad20a20
Balieiro004 <balieiro004@gmail.com>
2023-06-06 13:53:38 -0300

    GetAll, GetById, PutById, DeleteById and Exceptions

---
+++ ecommerce/src/test/java/br/com/compassuol/sp/challenge/ecommerce/service/ProductService.java
@@ -0,0 +1,15 @@
+package br.com.compassuol.sp.challenge.ecommerce.service;
+
+import br.com.compassuol.sp.challenge.ecommerce.controller.ProductController;
+import br.com.compassuol.sp.challenge.ecommerce.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ProductService {
+
+    private ProductService productService;
+    @BeforeEach
+    void setUo(){
+        productService = new ProductService();
+    }
+
+}
