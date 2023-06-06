b9b37ee1bac8c081f4e97e488f0b267e0892f09c
Alex Santana <alequeixam@gmail.com>
2023-06-06 11:33:44 -0300

    creating basic entity and fields

---
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/model/Customer.java
@@ -0,0 +1,84 @@
+package br.com.compassuol.sp.challenge.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 3)
+    private String name;
+
+    @Size(max = 11)
+    private String cpf;
+
+    @Email
+    private String email;
+
+    private boolean active;
+
+    public Customer(Long id, String name, String cpf, String email, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", email='" + email + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
---
+++ ecommerce/src/main/java/br/com/compassuol/sp/challenge/ecommerce/service/CustomerService.java
@@ -0,0 +1,12 @@
+package br.com.compassuol.sp.challenge.ecommerce.service;
+
+import br.com.compassuol.sp.challenge.ecommerce.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+
+}
